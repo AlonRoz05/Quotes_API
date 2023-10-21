c5f9bc6a754a005321352ac849e74c3c42c31c3f
AlonRoz <87911114+AlonRoz05@users.noreply.github.com>
2023-10-21 15:34:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-
 from transformers import pipeline
 from better_profanity import profanity
 import random
@@ -7,53 +6,35 @@ import json
 
 app = FastAPI()
 
-TAGS_PATH = "tags.json"
-with open(TAGS_PATH, "r") as file:
-    TAGS_DATA = json.load(file)
+tags_file_path = "tags.json"
+with open(tags_file_path, "r") as file:
+    tags_data = json.load(file)
 
-print(random.choice(TAGS_DATA["tags"]))
+profanity.load_censor_words()
 
-classifier = pipeline("text2text-generation", model="Rozi05/QuoteVibes_Model_Trained")
+model_name = "Rozi05/QuoteVibes_Model_Trained"
+classifier = pipeline("text2text-generation", model=model_name)
 
 @app.get("/")
 async def root():
-    return {"message": "Go to get-quote to generate a quote with a tag."}
+    return {"message": "Make sure your url is for the right page."}
 
 @app.get("/get-quote/{tag}")
 async def get_quote(tag: str):
-    for i in range(5):
-        if i == 0:
-            tag_1 = random.choice(TAGS_DATA["tags"])
-            while tag_1 == tag:
-                tag_2 = random.choice(TAGS_DATA["tags"])
-            
-        if i == 1:
-            tag_2 = random.choice(TAGS_DATA["tags"])
-            while tag_2 == tag_1 or tag_2 == tag:
-                tag_2 = random.choice(TAGS_DATA["tags"])
-                
-        elif i == 2:
-            tag_3 = random.choice(TAGS_DATA["tags"])
-            while tag_3 == tag_1 and tag_3 == tag_2 or tag_3 == tag:
-                tag_3 = random.choice(TAGS_DATA["tags"])
-        
-        elif i == 3:
-            tag_4 = random.choice(TAGS_DATA["tags"])
-            while tag_4 == tag_1 and tag_4 == tag_2 and tag_4 == tag_3 or tag_4 == tag:
-                tag_4 = random.choice(TAGS_DATA["tags"])
-        
-        elif i == 4:
-            tag_5 = random.choice(TAGS_DATA["tags"])
-            while tag_5 == tag_1 and tag_5 == tag_2 and tag_5 == tag_3 and tag_5 == tag_4 or tag_5 == tag:
-                tag_5 = random.choice(TAGS_DATA["tags"])
+    tagsInUse = []
+    for _ in range(5):
+        while True:
+            chosen_tag = random.choice(tags_data["tags"])
+            if (chosen_tag not in tagsInUse) and chosen_tag != tag:
+                tagsInUse.append(chosen_tag)
+                break
 
     if tag == "default":
-        tags = f"{tag_1};{tag_2};{tag_3};{tag_4};{tag_5}"
+        tags = f"{tagsInUse[0]};{tagsInUse[1]};{tagsInUse[2]};{tagsInUse[3]};{tagsInUse[4]}"
     else:
-        tags = f"{tag};{tag_1};{tag_2};{tag_3};{tag_4};{tag_5};{tag}"
+        tags = f"{tag};{tagsInUse[0]};{tagsInUse[1]};{tagsInUse[2]};{tagsInUse[3]};{tagsInUse[4]};{tag}"
 
     models_quote = classifier(tags)
-    profanity.load_censor_words()
 
     check_for_profanity = profanity.censor(models_quote[0]["generated_text"])
     test_for_text = models_quote[0]["generated_text"].replace(" ", "x")
