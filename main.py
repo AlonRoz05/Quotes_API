ca570354a2d052bbcf4a67992d69b6e578510e52
AlonRoz <87911114+AlonRoz05@users.noreply.github.com>
2023-10-27 17:08:29 +0300

    Bug fix and optimized the code.

--- main.py
+++ main.py
@@ -1,39 +1,46 @@
 from fastapi import FastAPI
-from transformers import pipeline
-from better_profanity import profanity
-import random
-import json
 from mangum import Mangum
+from requests import post
+from random import choice
+from json import load
 
-classifier = pipeline("text2text-generation", model="Rozi05/QuoteVibes_Model_Trained")
-profanity.load_censor_words()
+API_URL = "https://api-inference.huggingface.co/models/Rozi05/QuoteVibes_Model_Trained"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = post(API_URL, headers=headers, json=payload)
+	return response.json()
 
 with open("tags.json", "r") as file:
-    tags_data = json.load(file)
+    tags_data = load(file)
 
 app = FastAPI()
 handler = Mangum(app)
 
+@app.get("/")
+async def root():
+    return {"message" : "Make sure you on the right page."}
+
 @app.get("/get-quote/{tag}")
 async def get_quote(tag: str):
     tagsInUse = []
-    for i in range(5):
+    for _ in range(5):
         while True:
-            chosen_tag = random.choice(tags_data["tags"])
+            chosen_tag = choice(tags_data["tags"])
             if (chosen_tag not in tagsInUse) and chosen_tag != tag:
                 tagsInUse.append(chosen_tag)
                 break
 
+    tags = f"{tag};{tagsInUse[0]};{tagsInUse[1]};{tagsInUse[2]};{tagsInUse[3]};{tagsInUse[4]};{tag}"
+
     if tag == "default":
         tags = f"{tagsInUse[0]};{tagsInUse[1]};{tagsInUse[2]};{tagsInUse[3]};{tagsInUse[4]}"
-    else:
-        tags = f"{tag};{tagsInUse[0]};{tagsInUse[1]};{tagsInUse[2]};{tagsInUse[3]};{tagsInUse[4]};{tag}"
-        
-    while True:
-        models_quote = classifier(tags)
 
-        check_for_profanity = profanity.censor(models_quote[0]["generated_text"])
-        test_for_text = models_quote[0]["generated_text"].replace(" ", "x")
-        
-        if not ("*" in check_for_profanity) or test_for_text != "xxxxxxxxx":
-            return {"quote": models_quote[0]["generated_text"]}
+    while True:
+        models_quote = query({"inputs": tags})[0]["generated_text"]
+
+        # check for profanity
+        test_for_text = models_quote.replace(" ", "x")
+
+        if test_for_text != "xxxxxxxxx":
+            return {"quote": models_quote}
